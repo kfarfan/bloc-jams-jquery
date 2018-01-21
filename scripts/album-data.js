75ef0a6eb5b5481074e476460fc0c07ca402d9bf
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-01-21 13:32:31 -0500

    fixed playbuttons, and added RHCP songs

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -1,13 +1,24 @@
 const album = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  releaseInfo: '1909 Spanish Records',
-  albumArtUrl: 'assets/images/album_covers/01.png',
+  title: 'By The Way',
+  artist: 'Red Hot Chili Peppers',
+  releaseInfo: '2002 Warner Bros Records',
+  albumArtUrl: 'assets/images/album_covers/rhcp.png',
   songs: [
-    { title: 'Blue', duration: '161.71', soundFileUrl: 'assets/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', soundFileUrl: 'assets/music/green.mp3' },
-    { title: 'Red', duration: '268.45', soundFileUrl: 'assets/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', soundFileUrl: 'assets/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', soundFileUrl: 'assets/music/magenta.mp3' }
+    { title: 'By The Way', duration: '161.71', soundFileUrl: 'assets/music/bytheway.mp3' },
+    { title: 'Universally Speaking', duration: '103.96', soundFileUrl: 'assets/music/universallyspeaking.mp3' },
+    { title: 'This Is The Place', duration: '268.45', soundFileUrl: 'assets/music/thisistheplace.mp3' },
+    { title: 'Dosed', duration: '153.14', soundFileUrl: 'assets/music/dosed.mp3' },
+    { title: 'Don\'t Forget Me', duration: '374.22', soundFileUrl: 'assets/music/dontforgetme.mp3' },
+    { title: 'The Zephyr Song', duration: '374.22', soundFileUrl: 'assets/music/zephyrsong.mp3' },
+    { title: 'Can\'t Stop', duration: '374.22', soundFileUrl: 'assets/music/cantstop.mp3' },
+    { title: 'I Could Die For You', duration: '374.22', soundFileUrl: 'assets/music/icoulddieforyou.mp3' },
+    { title: 'Midnight', duration: '374.22', soundFileUrl: 'assets/music/midnight.mp3' },
+    { title: 'Throw Away Your Television', duration: '374.22', soundFileUrl: 'assets/music/throwawayyourtelevision.mp3' },
+    { title: 'Cabron', duration: '374.22', soundFileUrl: 'assets/music/cabron.mp3' },
+    { title: 'Tear', duration: '374.22', soundFileUrl: 'assets/music/tear.mp3' },
+    { title: 'On Mercury', duration: '374.22', soundFileUrl: 'assets/music/onmercury.mp3' },
+    { title: 'Minor Thing', duration: '374.22', soundFileUrl: 'assets/music/minorthing.mp3' },
+    { title: 'Warm Tape', duration: '374.22', soundFileUrl: 'assets/music/warmtape.mp3' },
+    { title: 'Venice Queen', duration: '374.22', soundFileUrl: 'assets/music/venicequeen.mp3' }
   ]
 };
